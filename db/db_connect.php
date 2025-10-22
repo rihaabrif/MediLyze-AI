6dca05d673bcfaa2f4c6ab79000e6bf3a61d399e
Rihaab Rifqi <rihaabr.rrmedia@gmail.com>
2025-10-22 17:59:34 +0530

    db can now be accessed in any localhost

--- db/db_connect.php
+++ db/db_connect.php
@@ -4,7 +4,7 @@ define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', ''); // Your DB password, if you have one
 define('DB_NAME', 'medilyze_db');
-define('DB_PORT', 3307);
+define('DB_PORT', 3306);
 
 // Attempt to connect to MySQL database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
